f9c68741868914a877599edd7230ca9773691b41
Marcelo Norberto <norbertosouza.marcelo@outlook.com>
2024-11-21 21:20:59 -0300

    refactor: remove notion module

--- AgileIntegration.Modules.Notion/UseCases/CreateRow/CreateRowUseCaseInput.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AgileIntegration.Modules.Notion.UseCases.CreateRow;
-
-public class CreateRowUseCaseInput
-{
-    public int Ordem { get; set; }
-    public string Name { get; set; }
-    public string Period { get; set; }
-    public string Priority { get; set; }
-}
