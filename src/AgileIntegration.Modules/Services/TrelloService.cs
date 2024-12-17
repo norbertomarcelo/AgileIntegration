f424cbcc81affae75da3f8637b7dbffa2691cc7f
Marcelo Norberto <norbertosouza.marcelo@outlook.com>
2024-12-16 21:11:38 -0300

    feat: add Trello service

---
+++ src/AgileIntegration.Modules/Services/TrelloService.cs
@@ -0,0 +1,22 @@
+﻿using AgileIntegration.Modules.Dtos;
+using AgileIntegration.Modules.Interfaces;
+
+namespace AgileIntegration.Modules.Services;
+
+public class TrelloService : ICommonServices
+{
+    public Task<CreateTaskOutput> CreateTask(CreateTaskInput input)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> TaskAlreadyExists(CreateTaskInput input)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool ValidateTask(CreateTaskInput input, out string errorMessage)
+    {
+        throw new NotImplementedException();
+    }
+}
